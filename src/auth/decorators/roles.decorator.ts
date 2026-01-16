6900247134dc310c1ce7ce87d69f2ad8f38cf5f6
froddodev <froddo.dev@gmail.com>
2026-01-16 18:41:10 -0300

    refactor: separate ROLES_KEY constant from Roles decorator

--- src/auth/decorators/roles.decorator.ts
+++ src/auth/decorators/roles.decorator.ts
@@ -1,7 +1,7 @@
 import { SetMetadata } from '@nestjs/common';
 import { UserRole } from '../../user/enums/user-role.enum';
+import { ROLES_KEY } from '../constants/roles.constant';
 
-export const ROLES_KEY = 'roles';
 export const Roles = (...roles: UserRole[]) => {
   return SetMetadata(ROLES_KEY, roles);
 };
